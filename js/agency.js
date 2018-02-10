4621629c5d542bc70bd8c16d6790ba7548a680de
Matthias Schnetzer <mschnetz@wu.ac.at>
2018-02-10 12:26:25 +0100

    rebuilding site Sa 10 Feb 2018 12:26:25 CET

--- js/agency.js
+++ js/agency.js
@@ -10,6 +10,15 @@ $(document).ready(function(){
         window.location.hash = hash;
       });
     }
+    if (this.hash && window.location.pathname == "/en/") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
   });
 });
 
