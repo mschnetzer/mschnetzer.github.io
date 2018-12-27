de7c4679bd1564081ffdc24f410ada82c251c059
Matthias Schnetzer <mschnetz@wu.ac.at>
2018-12-27 11:21:37 +0100

    rebuilding site Do 27 Dez 2018 11:21:37 CET

--- js/agency.js
+++ js/agency.js
@@ -54,23 +54,3 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
-// Async contact form
-$('form[id=contactForm]').submit(function(){
-  $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (res.code == "200")
-      $('form[id=contactForm] #success').show();
-    }).fail(function(){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    $('form[id=contactForm] #error').show();
-  });
-  return false;
-});
-
-// Contact form validation
-$.validate({
-  modules : 'html5, toggleDisabled'
-});
